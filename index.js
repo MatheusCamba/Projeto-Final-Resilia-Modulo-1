c8d10706ffc15f47bac4c3892fafad19908acd30
Matheus Camba <mhscamba@gmail.com>
2021-11-24 16:10:29 -0300

    Os códigos do HTML e CSS foram reorganizados, em relação as classes e id's.

--- index.js
+++ index.js
@@ -1,15 +1,1 @@
-function escolherPersonagem(numeroDoPersonagem) {
-    
-    var escolhaDePersonagem = prompt('Digite 1 para escolher Robson \n 2 para Paulinho \n 3 para Jamil')
-    if (escolhaDePersonagem == 1) {
-        return window.location.assign('./robson.html')
-    } else if (escolhaDePersonagem == 2) {
-        return window.location.assign('./paulinho.html')
-    } else if (escolhaDePersonagem == 3) {
-        return window.location.assign('./jamil.html')
-    } else {
-        alert('Escolha o personagem !!!')
-    }
-}
-
-escolherPersonagem();
+alert("É recomendável o uso do Google Maps para melhor experiência.")
